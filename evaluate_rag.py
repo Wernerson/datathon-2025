e92b74978c78fc7a0f391abb03465dc94c8c99e5
Harald Semmelrock <h.semmelrock@student.tugraz.at>
2025-04-06 02:12:38 +0200

    check2

---
+++ evaluate_rag.py
@@ -0,0 +1,11 @@
+# Query and Ground truth pairs
+# The ground_truth is the information
+
+
+query1 = "What are the assisted living private rooms at covenantwood equipped with?"
+link1 = "https://covenantwoods.com/health-services/assisted-living/"
+ground_truth_1 = "Private bath with large walk-in shower (handicapped accessible), Large windows and plenty of natural light, Individually controlled lighting, heating, and air conditioning
+Cable television and Wi-Fi service
+Housekeeping and linen service
+Emergency call system
+"
