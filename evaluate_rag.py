6a6e9ff61be94fda754ebfb5507883ae6e5a0866
Harald Semmelrock <h.semmelrock@student.tugraz.at>
2025-04-06 07:03:24 +0200

    evaluate

--- evaluate_rag.py
+++ evaluate_rag.py
@@ -1,11 +1,53 @@
-# Query and Ground truth pairs
-# The ground_truth is the information
-
-
-query1 = "What are the assisted living private rooms at covenantwood equipped with?"
-link1 = "https://covenantwoods.com/health-services/assisted-living/"
-ground_truth_1 = "Private bath with large walk-in shower (handicapped accessible), Large windows and plenty of natural light, Individually controlled lighting, heating, and air conditioning
-Cable television and Wi-Fi service
-Housekeeping and linen service
-Emergency call system
-"
+# Query and Ground truth pairs including link
+qa_dict = {
+    1: {
+        "query": "What are the assisted living private rooms at covenantwood equipped with?",
+        "link": "https://covenantwoods.com/health-services/assisted-living/",
+        "answer": "Private bath with large walk-in shower (handicapped accessible), Large windows and plenty of natural light, Individually controlled lighting, heating, and air conditioning, Cable television and Wi-Fi service, Housekeeping and linen service, Emergency call system"
+    },
+    2: {
+        "query": "What therapy services do covenantwood offer?",
+        "link": "https://covenantwoods.com/health-services/therapy-services/",
+        "answer": "Specialized certifications and treatment, In-patient short-term rehabilitation to power your recovery, Convenient outpatient therapy services for Covenant Woods residents"
+    },
+    3: {
+        "query": "Who is part of the CovenantWood Management team?",
+        "link": "https://covenantwoods.com/about-us/leadership/",
+        "answer": "Thom Wright, President & CEO; Juanita Parks, CFO; etc."
+    },
+    4: {
+        "query": "What company provides assisted living near Richmond, Virginia?",
+        "link": "---",
+        "answer": "covenantwoods.com (and others possible)"
+    },
+    5: {
+        "query": "What companies uses packaging materials in Valencia, California?",
+        "link": "---",
+        "answer": "amsfulfillment.com (and others possible)"
+    },
+    6: {
+        "query": "Who uses Agile Methodologies to deal with Marketing in Fort Lauderdale, FL?",
+        "link": "---",
+        "answer": "starmark.com (and others possible)"
+    },
+    7:{
+        "query": "How many orders does amsfulfillment process per year?",
+        "link": "https://www.amsfulfillment.com/",
+        "answer": "8.36 million"
+    },
+    8:{
+        "query": "How can i contact American Cruise Lines?",
+        "link": "https://www.americancruiselines.com/",
+        "answer":"By phone 800-460-4518"
+    },
+    8:{
+        "query": "How can i contact American Cruise Lines?",
+        "link": "https://www.americancruiselines.com/",
+        "answer":"By phone 800-460-4518 or mail Inquiry@AmericanCruiseLines.com"
+    },
+    9:{
+        "query": "What classes of ships does American Cruise Lines have on offer?",
+        "link": "https://www.americancruiselines.com/usa-riverboat-cruise-ships",
+        "answer":" Patriot Class, Coastal Cats, Classic Paddlewheelers, Constellation Class Ships, Independence Class, and the only modern American Riverboats."
+    }
+}
